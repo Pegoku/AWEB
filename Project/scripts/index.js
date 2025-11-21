901669ee48f3de146887911974aaf506f8e9e494
Pegoku <pegoku08@gmail.com>
2025-11-21 12:07:39 +0100

    working sticky

--- Project/scripts/index.js
+++ Project/scripts/index.js
@@ -1,5 +1,5 @@
 $(window).on('scroll',function(){
-  var headerXPosition = Math.min(Math.round($(window).scrollTop()/20), 6);
+  var headerXPosition = Math.min(Math.round($(window).scrollTop()/15), 3);
   console.log(headerXPosition);
-  $('.actionDiv').css('transform','translateX('+(headerXPosition)+'%)');
+  $('.actionDiv').css('transform','translateX('+(headerXPosition)+'rem)');
 });
