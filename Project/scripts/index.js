fa9231dc44f18b8c9fbc635e61b2b370e76ded05
Pegoku <pegoku08@gmail.com>
2025-11-26 11:18:29 +0100

    finish mobile

--- Project/scripts/index.js
+++ Project/scripts/index.js
@@ -1,7 +1,15 @@
-$(window).on('scroll',function(){
-  var scroll = $(window).scrollTop();
-  var progress = Math.min(scroll / 90, 1);
-  var headerXPosition = Math.round((1 - (1 - progress) * (1 - progress)) * 90); // 90(1-(1-x)**2)
-  console.log(headerXPosition);
-  $('.actionDiv').css({'transform':'translateX('+(headerXPosition)+'px)', 'width':'calc(100% - '+(headerXPosition)+'px)'});
+$(window).on("scroll", function () {
+  if ($(window).width() <= 768) {
+    // console.log($(window).width());
+    var scroll = $(window).scrollTop();
+    var progress = Math.min(scroll / 90, 1);
+    var headerXPosition = Math.round(
+      (1 - (1 - progress) * (1 - progress)) * 90
+    ); // 90(1-(1-x)**2)
+    console.log(headerXPosition);
+    $(".actionDiv").css({
+      transform: "translateX(" + headerXPosition + "px)",
+      width: "calc(100% - " + headerXPosition + "px)",
+    });
+  }
 });
